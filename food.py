b05b3c61d8c9cdf914244b4af8256b53e26de653
Dormant_Hero <mgo3kakashi@gmail.com>
2023-10-13 21:11:42 +0100

    Added the following:
    -Menu with the play game option only working.
    -Music (one track only)
    -Snake controlled by arrow keys on the keyboard
    -Food the snake can eat that respawns and makes the snake longer

--- food.py
+++ food.py
@@ -1,28 +1,30 @@
 import random
 from screen import Screen
 from turtle import Turtle
+from music import Music
+
+music = Music()
+
 
 class Food(Turtle):
     def __init__(self):
         super().__init__()
         self.food_objects = []
 
-
-
-
     def move_food(self):
         screen = Screen()
         x_location = random.randint(0, screen.screen_width - 1280)
         y_location = random.randint(0, screen.screen_height - 720)
         self.food_objects[0].setposition(x_location, y_location)
+        music.munch()
 
     def construct_food(self):
         food = Turtle("circle")
         food.color("red")
         food.penup()
         screen = Screen()
-        x_location = random.randint(0, screen.screen_width - 1280)
-        y_location = random.randint(0, screen.screen_height - 720)
+        x_location = random.randint(900, screen.screen_width - 1280)
+        y_location = random.randint(600, screen.screen_height - 720)
         food.setposition(x_location, y_location)
         self.food_objects.append(food)
-        self.move_food()
+
--- music.py
+++ music.py
@@ -1,11 +1,20 @@
-#background music
+#All the sounds of the game are here
+import time
+
+import pygame
+import os
 
 class Music():
-    import pygame
     def __init__(self):
-        self.pygame.init()
-        self.pygame.mixer.music.load("Music/roll-the-tape.mp3")
-        self.pygame.mixer.music.play(loops=-1)
+        pygame.init()
+        pygame.mixer.music.load("Music/roll-the-tape.mp3")
+        pygame.mixer.music.play(loops=-1)
+        pygame.mixer.music.set_volume(0.3)
+        self.munch_sound = pygame.mixer.Sound("Music/nom.mp3")
+
+    def munch(self):
+        self.munch_sound.play().set_volume(1)
+
 
 
 
